dc4b1b9455c512069e275c30940c06fc11cfcc3a
Arthur Emanuel Dala Libera <arthuremanueldalalibera@gmail.com>
2022-05-30 10:54:43 -0300

    Conceitos Intent
    Conceitos Intent foi movido para o Git para poder fazer alterações futuras 30/05/2022

---
+++ conceitos_intent/app/src/main/java/formacao/desenvolvedores/tecnologia/projetodesoftwareorientadoaobjetos/conceitosintent/HelloWorld.java
@@ -0,0 +1,8 @@
+package formacao.desenvolvedores.tecnologia.projetodesoftwareorientadoaobjetos.conceitosintent;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
