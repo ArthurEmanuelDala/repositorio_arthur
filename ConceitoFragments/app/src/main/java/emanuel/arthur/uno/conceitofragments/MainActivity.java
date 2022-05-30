d28b74bed27988ac590a9ee5d9df13deb91be075
Arthur Emanuel Dala Libera <arthuremanueldalalibera@gmail.com>
2022-05-30 10:51:30 -0300

    Update MainActivity.java

--- ConceitoFragments/app/src/main/java/emanuel/arthur/uno/conceitofragments/MainActivity.java
+++ ConceitoFragments/app/src/main/java/emanuel/arthur/uno/conceitofragments/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends FragmentActivity implements ToolbarFragment.To
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
- 
+
     }
 
 
