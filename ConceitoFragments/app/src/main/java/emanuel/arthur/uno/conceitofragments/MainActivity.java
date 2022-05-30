db55d5f48b5a3fc41f2a29ac269c94502e20620d
Arthur Emanuel Dala Libera <arthuremanueldalalibera@gmail.com>
2022-05-30 08:31:11 -0300

    Primerio commit
    Commit inicial do projeto em aula 30/05/2022

---
+++ ConceitoFragments/app/src/main/java/emanuel/arthur/uno/conceitofragments/MainActivity.java
@@ -0,0 +1,18 @@
+package emanuel.arthur.uno.conceitofragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ ConceitoFragments/app/src/main/java/emanuel/arthur/uno/conceitofragments/TextoFragment.java
@@ -0,0 +1,27 @@
+package emanuel.arthur.uno.conceitofragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class TextoFragment extends Fragment {
+    private TextView tvTexto;
+
+    public TextoFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View textoLayoutInflated = inflater.inflate(R.layout.fragment_texto, container, false);
+
+        tvTexto = textoLayoutInflated.findViewById(R.id.tvTexto);
+
+        return textoLayoutInflated;
+    }
+}
