21afada7c8559bca5815ed56bb308115691a86e7
ArthurEmanuelDala <arthuremanueldalalibera@gmail.com>
2022-06-27 11:19:29 -0300

    Conceito Framework Classes e Abstrações e Interfaces
    Alterações e criações 27/06/2022

--- ConceitosClassesAbstrataseInterfaces/app/src/test/java/emanuel/arthur/uno/conceitosclassesabstrataseinterfaces/ExampleUnitTest.java
+++ ConceitosClassesAbstrataseInterfaces/app/src/test/java/emanuel/arthur/uno/conceitosclassesabstrataseinterfaces/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import emanuel.arthur.uno.conceitosclassesabstrataseinterfaces.formas.Triangulo;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +15,12 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void areaTrianguloTest(){
+        Triangulo triangulo = new Triangulo(5, 6, 7);
+        boolean x = triangulo.area() > 0;
+        assertTrue(x);
     }
 }
