4a2e522da9fd133f9f8f426f4d1e4dee79308a0f
ArthurEmanuelDala <arthuremanueldalalibera@gmail.com>
2022-09-14 11:15:26 -0300

    Projeto com javascript
    Criação de javascripts´s com html

---
+++ Projetos HTML com JavaScript/Aula 14.09.2022/js/script.js
@@ -0,0 +1,12 @@
+//alert("Oi mundo");
+
+/*document.write("<h1> Este é um cabeçalho</h1>");
+document.write("<p>Isto é um parágrafo</p>");
+document.write("<p>Este é outro parágrafo</p>");*/
+
+//window.alert("Senha incorreta, acesso negado");
+//alert("Seha negada");
+
+nome=prompt("Qual o seu nome?");
+
+soma=prompt("O Resultado da soma é")
